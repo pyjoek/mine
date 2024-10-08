9c0ff2342be1d933b2232b4b05a9e3dc041c185d
pyyoek <joelnrlson@gmai.com>
2024-10-08 18:14:34 +0300

    almost done cooking

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyHome extends StatefulWidget {
 }
 
 class Myhome extends State<MyHome> {
-  Color bg = Colors.black;
+  Color bg = Color.fromARGB(255, 26, 26, 26);
   Color ft = Colors.white;
   Color box = Color.fromARGB(115, 72, 72, 72);
   int count = 0;
@@ -34,7 +34,7 @@ class Myhome extends State<MyHome> {
           backgroundColor: bg,
           actions: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               child: InkWell(
                 child: Icon(Icons.brightness_low_outlined,
                 color: ft,
@@ -48,8 +48,8 @@ class Myhome extends State<MyHome> {
                       box = Color.fromARGB(115, 204, 204, 204);
                     }else {
                       ft = Colors.white;
-                      bg = Colors.black;
-                      box = Color.fromARGB(115, 72, 72, 72);
+                      bg = Color.fromARGB(255, 26, 26, 26);
+                      box = Color.fromARGB(115, 216, 14, 14);
                     }
                   })
                 },
@@ -67,9 +67,29 @@ class Myhome extends State<MyHome> {
                 radius: 50,
                 backgroundColor: Colors.orange,
               ),
-              SizedBox(height: 20),
-              Text("JOEL JACKSON", style: TextStyle(color: ft, fontSize: 25,fontWeight: FontWeight.w700),),
-              SizedBox(height: 10,),
+              SizedBox(height: 3),
+              Text("JOEL JACKSON", style: TextStyle(color: ft, fontSize: 17,fontWeight: FontWeight.w700),),
+              SizedBox(height: 3),
+              Text("joelnrlson@gmail.com", style: TextStyle(color: ft, fontSize: 10),),        
+              SizedBox(height: 15,),
+              Container(
+                width: 170,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Upgrade to PRO", style: TextStyle(fontWeight: FontWeight.bold),)
+                    ),
+                  ),
+                )
+              ),
+              SizedBox(height: 25,),
               Container(
                 width: 300,
                 height: 40,
@@ -84,7 +104,7 @@ class Myhome extends State<MyHome> {
                       padding: const EdgeInsets.all(8.0),
                       child: Row(
                         children: [
-                          Icon(Icons.privacy_tip, color: ft,),
+                          Icon(Icons.privacy_tip_outlined, color: ft,),
                           SizedBox(width: 4,),
                           Text("Private Account", style: TextStyle(color: ft),),
                           Spacer(),
@@ -94,7 +114,137 @@ class Myhome extends State<MyHome> {
                     ),
                   ),
                 )
-              )
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(115, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.history_rounded, color: ft,),
+                          SizedBox(width: 4,),
+                          Text("Purchase History", style: TextStyle(color: ft),),
+                          Spacer(),
+                          Icon(Icons.arrow_right_sharp, color: ft,)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(115, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.help_outline, color: ft,),
+                          SizedBox(width: 4,),
+                          Text("Help & Support", style: TextStyle(color: ft),),
+                          Spacer(),
+                          Icon(Icons.arrow_right_sharp, color: ft,)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(115, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.settings, color: ft,),
+                          SizedBox(width: 4,),
+                          Text("Settings", style: TextStyle(color: ft),),
+                          Spacer(),
+                          Icon(Icons.arrow_right_sharp, color: ft,)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(115, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.supervised_user_circle_outlined, color: ft,),
+                          SizedBox(width: 4,),
+                          Text("Invite a friend", style: TextStyle(color: ft),),
+                          Spacer(),
+                          Icon(Icons.arrow_right_sharp, color: ft,)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(115, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.logout_rounded, color: ft,),
+                          SizedBox(width: 4,),
+                          Text("Logout", style: TextStyle(color: ft),),
+                          Spacer(),
+                          Icon(Icons.arrow_right_sharp, color: ft,)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
                 ],
               ),
             )
