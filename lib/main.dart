103ac6cd7b4e3ecd99dacd3ed611a60a9f0b9f97
pyyoek <joelnrlson@gmai.com>
2024-10-08 14:02:00 +0300

    added the name part

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,28 +21,85 @@ class MyHome extends StatefulWidget {
 class Myhome extends State<MyHome> {
   Color bg = Colors.black;
   Color ft = Colors.white;
+  Color box = Color.fromARGB(115, 72, 72, 72);
+  int count = 0;
 
   @override 
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: bg,
         appBar: AppBar(
           backgroundColor: bg,
           actions: [
-            InkWell(
-              child: Icon(Icons.home,
-              color: ft,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                child: Icon(Icons.brightness_low_outlined,
+                color: ft,
+                ),
+                onTap: () => {
+                  setState(() {
+                    count += 1;
+                    if (count % 2 == 0) {
+                      bg = Colors.white;
+                      ft = Colors.black;
+                      box = Color.fromARGB(115, 204, 204, 204);
+                    }else {
+                      ft = Colors.white;
+                      bg = Colors.black;
+                      box = Color.fromARGB(115, 72, 72, 72);
+                    }
+                  })
+                },
               ),
-              onTap: () => {
-                setState(() {
-                  bg = Colors.white;
-                  ft = Colors.black;
-                })
-              },
             )
           ],
         ),
+        body: ListView(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                child: Text("J"),
+                radius: 50,
+                backgroundColor: Colors.orange,
+              ),
+              SizedBox(height: 20),
+              Text("JOEL JACKSON", style: TextStyle(color: ft, fontSize: 25,fontWeight: FontWeight.w700),),
+              SizedBox(height: 10,),
+              Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(115, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: InkWell(
+                  onTap: () => print("You clicked nigga"),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.privacy_tip, color: ft,),
+                          SizedBox(width: 4,),
+                          Text("Private Account", style: TextStyle(color: ft),),
+                          Spacer(),
+                          Icon(Icons.arrow_right_sharp, color: ft,)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              )
+                ],
+              ),
+            )
+          ],
+        )
       ),
     );
   }
